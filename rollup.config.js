8fc85c1756b307879fb7d9348220e0088c2ba790
wangming <ming.wang24@gientech.com>
2025-03-24 20:54:01 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default [
 	{
 		input: 'src/main.js',
 		output: {
-			name: 'howLongUntilLunch',
+			name: 'main',
 			file: pkg.browser,
 			format: 'umd'
 		},
--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
 
-export default function howLongUntilLunch(hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
-
-	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
-}
+console.log('%c [  ]-2', 'font-size:13px; background:pink; color:#bf2c9f;', )
